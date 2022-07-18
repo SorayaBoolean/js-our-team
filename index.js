439a7b8a98992f7059af0831aa0392a78ccbed06
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-18 16:21:14 +0200

    add: created array of objects with each team's component in js file

--- index.js
+++ index.js
@@ -7,6 +7,38 @@
 //Bonus1: Trasformare la stringa foto in una immagine effettiva
 //Bonus2: Organizzare i singoli membri in card/schede
 
-//Creo array di Oggetti:
+//Creo Array "teamComponents" composto al suo interno da Oggetti:
 
-const team = 
+const teamComponents = [
+
+    {'Nome': 'Wayne Barnett',
+     'Ruolo': 'Founder & CEO',
+     'Foto' : 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {'Nome': 'Angela Caroll',
+     'Ruolo': 'Chief Editor',
+     'Foto' : 'angela-caroll-chief-editor.jpg'
+    },
+
+    {'Nome': 'Walter Gordon',
+     'Ruolo': 'Office Manager',
+     'Foto' : 'walter-gordon-office-manager.jpg'
+    },
+
+    {'Nome': 'Angela Lopez',
+     'Ruolo': 'Social Media Manager',
+     'Foto' : 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {'Nome': 'Scott Estrada',
+     'Ruolo': 'Developer',
+     'Foto' : 'scott-estrada-developer.jpg'
+    },
+
+    {'Nome': 'Barbara Ramos',
+     'Ruolo': 'Graphic Designer',
+     'Foto' : 'barbara-ramos-graphic-designer.jpg'
+    }
+
+];
