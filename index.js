297523ff67b5456789d3af4d9276b04e893df890
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-18 16:09:49 +0200

    steps in javascript starting creating an array made of objects

---
+++ index.js
@@ -0,0 +1,12 @@
+//STEPS:
+//1.Creare l’array di oggetti con le informazioni fornite.
+//2.Stampare su console le informazioni di nome, ruolo e la stringa della foto
+//3.Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+//Bonus1: Trasformare la stringa foto in una immagine effettiva
+//Bonus2: Organizzare i singoli membri in card/schede
+
+//Creo array di Oggetti:
+
+const team = 
